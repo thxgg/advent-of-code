2b1863414d7b8eff8899ad70ed3e821c84558e86
Georgi Georgiev <georgigeorgievofficial@gmail.com>
2023-07-30 21:56:09 +0300

    2022 Day 01

---
+++ 2022/01/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func part_one() {
+	max := 0
+
+	for _, elf := range strings.Split(input, "\n\n") {
+		calories := 0
+
+		for _, food := range strings.Split(elf, "\n") {
+			n, _ := strconv.Atoi(food)
+			calories += n
+		}
+
+		if calories > max {
+			max = calories
+		}
+	}
+
+	fmt.Println(max)
+}
+
+func part_two() {
+	var elves []int
+
+	for _, elf := range strings.Split(input, "\n\n") {
+		calories := 0
+
+		for _, food := range strings.Split(elf, "\n") {
+			n, _ := strconv.Atoi(food)
+			calories += n
+		}
+
+		elves = append(elves, calories)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(elves)))
+	sum := 0
+	for _, val := range elves[:3] {
+		sum += val
+	}
+	fmt.Println(sum)
+}
+
+func main() {
+	// part_one()
+	part_two()
+}
